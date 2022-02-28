06d2a6cf436fffac74f519ffb51830321b13206a
PrantaDas <prantodas043@gmail.com>
2022-03-01 02:31:09 +0600

    details section added

--- js/index.js
+++ js/index.js
@@ -15,22 +15,66 @@ const searchPhone=()=>{
 
 const displaySearchResult=phones=>{
     const resultContainer=document.getElementById('search-result');
-    resultContainer.innerHTML='';
+    // resultContainer.innerHTML='';
     phones.forEach(phone=>{
         console.log(phone);
+        // if(phone.length<21){
+        //     const div=document.createElement('div');
+        //     div.classList.add('col');
+        //     div.innerHTML=`
+        //     <div class="card h-100 p-3">
+        //     <img src="${phone.image}" class="card-img-top" alt="...">
+        //     <div class="card-body d-flex flex-column align-items-start">
+        //       <h5 class="card-title text-info">Phone Name: ${phone.phone_name}</h5>
+        //       <p class="card-text fw-bolder text-info">Brand: ${phone.brand}</p>
+        //       <button onclick="loadPhoneById('${phone.slug}')" type="button" class="btn btn-primary">Details</button>
+        //     </div>
+        //   </div>
+    
+        //     `;
+        //     resultContainer.appendChild(div);
+        // }
         const div=document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
-        <div class="card h-100 p-3">
+        <div class="card h-100 p-3 d-flex flex-column align-items-center">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body d-flex flex-column align-items-start">
           <h5 class="card-title text-info">Phone Name: ${phone.phone_name}</h5>
           <p class="card-text fw-bolder text-info">Brand: ${phone.brand}</p>
-          <a href="" class="btn btn-primary">Details</a>
+          <div class="text-center">
+          <button onclick="loadPhoneById('${phone.slug}')" type="button" class="btn btn-primary ">Details</button></div>
         </div>
       </div>
 
         `;
         resultContainer.appendChild(div);
     })
+}
+
+const loadPhoneById=phoneId=>{
+    console.log(phoneId);
+    const url=` https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+    .then(response=>response.json())
+    .then(json=>displayPhoneById(json.data))
+}
+
+const displayPhoneById=phone=>{
+    console.log(phone);
+    const phoneDetails=document.getElementById('phone-details');
+    const div=document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML=`
+    <div class="card h-100 p-3">
+    <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body d-flex flex-column align-items-start">
+      <h5 class="card-title text-info">Phone Name: ${phone.phone_name}</h5>
+      <p class="card-text fw-bolder text-info">Brand: ${phone.brand}</p>
+    </div>
+  </div>
+    
+  
+    `;
+    phoneDetails.appendChild(div);
 }
