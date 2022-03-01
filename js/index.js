8ecf1870648f641c4cfa027fec7be15abda6ec21
PrantaDas <prantodas043@gmail.com>
2022-03-01 15:27:41 +0600

    error handled

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const searchPhone=()=>{
-    
+    const phoneDetails=document.getElementById('phone-details');
+    phoneDetails.innerHTML='';
     const searchField=document.getElementById('search-phone');
     const searchText=searchField.value;
     console.log(searchText);
@@ -11,35 +12,39 @@ const searchPhone=()=>{
 }
 
 
-const displaySearchResult=phones=>{
+const displaySearchResult=(phones)=>{
     const resultContainer=document.getElementById('search-result');
-    // resultContainer.innerHTML='';
-    if(phones){
+    resultContainer.innerHTML='';
+    console.log(phones);
+    if(phones.length==''){
         document.getElementById('error-message').style.display='block';
     }
-    if(phones.length>=20||phones.length<20){
+    else{
         document.getElementById('error-message').style.display='none';
-        resultContainer.innerHTML='';
-        const part=phones.slice(0,20);
-        part.forEach(phone=>{
-        console.log(phone);
-        const div=document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML=`
-        <div class="card h-100 p-3 d-flex flex-column align-items-center">
-        <img src="${phone.image}" class="card-img-top" alt="...">
-        <div class="card-body d-flex flex-column align-items-start">
-          <h5 class="card-title text-info">Phone Name: ${phone.phone_name}</h5>
-          <p class="card-text fw-bolder text-info">Brand: ${phone.brand}</p>
-          <div class="text-center">
-          <button onclick="loadPhoneById('${phone.slug}')" type="button" class="btn btn-primary ">Details</button></div>
-        </div>
-      </div>
-
-        `;
-        resultContainer.appendChild(div);
-    })
+        if(phones.length>=20||phones.length<20){
+            
+            resultContainer.innerHTML='';
+            const part=phones.slice(0,20);
+            part?.forEach(phone=>{
+            const div=document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML=`
+            <div class="card h-100 p-3 d-flex flex-column align-items-center">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body d-flex flex-column align-items-start">
+              <h5 class="card-title text-info">Phone Name: ${phone.phone_name}</h5>
+              <p class="card-text fw-bolder text-info">Brand: ${phone.brand}</p>
+              <div class="text-center">
+              <button onclick="loadPhoneById('${phone.slug}')" type="button" class="btn btn-primary ">Details</button></div>
+            </div>
+          </div>
+    
+            `;
+            resultContainer.appendChild(div);
+        })
     }
+    }
+    
 }
 
 const loadPhoneById=phoneId=>{
